a9581bb6f6c008fb4fe962549a49b96d72db8ecd
dsdr <dev@damndev.com>
2017-01-24 09:06:39 +0300

    first working release, trying to pass Wallie

--- pprint_json.py
+++ pprint_json.py
@@ -1,13 +1,28 @@
 import json
+import argparse
+
+def parser_config():
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.RawDescriptionHelpFormatter,
+        description='''\
+            Read data from json file, set in first argument.
+            Print it in pretty json format''')
+    parser.add_argument('file_path', metavar='data.json', type=str,
+                        help='path to the json file')
+    return parser
 
 
-def load_data(filepath):
-    pass
+def load_json_data(file_path):
+    with open(file_path) as json_data:
+        return json.load(json_data)
 
 
 def pretty_print_json(data):
-    pass
+    return json.dumps(data, sort_keys=True, ensure_ascii=False,
+               indent = 4, separators = (',', ': '))
 
 
 if __name__ == '__main__':
-    pass
+    bars = load_json_data(parser_config().parse_args().file_path)
+    pretty_json_data = pretty_print_json(bars)
+    print(pretty_json_data)
